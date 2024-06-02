8058fabe60e7b5a90f77ec9a898eef8d498f4c77
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-02 14:20:27 +0200

    Create solution for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayBefore) {
+    let arrayAfter = arrayBefore;
+
+    for (let i = 1; i < arguments.length; i++) {
+        
+        // If a value is in the array multiple times, all need to be deleted
+        while (arrayAfter.indexOf(arguments[i]) !== -1) {
+            const index = arrayAfter.indexOf(arguments[i])
+            if (index > -1) {
+                arrayAfter.splice(index, 1);
+            } 
+        }
+    }
+
+    return arrayAfter;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+removeFromArray([1,2,3,4], 3);
