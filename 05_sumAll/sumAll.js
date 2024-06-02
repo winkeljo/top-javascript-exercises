cd704f1f291a6eb6bb377b41f0baef94ae2dddf1
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-02 14:38:29 +0200

    Create solution for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (typeof startNumber !== "number" || typeof endNumber !== "number") return "ERROR";
+    if (startNumber < 0 || endNumber < 0) return "ERROR";
 
+    let sum = 0;
+    if (startNumber > endNumber) {
+        const backupNumber = startNumber;
+        startNumber = endNumber;
+        endNumber = backupNumber;
+    }
+
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
