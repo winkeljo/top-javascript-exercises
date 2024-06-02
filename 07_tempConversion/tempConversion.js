c5d4b406ca98735418c67293e6e9f5725e8ee4ce
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-02 21:56:15 +0200

    Create solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+
+  if (typeof temperatureFahrenheit !== "number") return "ERROR";
+
+  return Math.round(((temperatureFahrenheit - 32) * (5 / 9)) * 10) / 10;
+  -32*5/9
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(temperatureCelsius) {
+
+  if (typeof temperatureCelsius !== "number") return "ERROR";
+
+  return Math.round(((temperatureCelsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
