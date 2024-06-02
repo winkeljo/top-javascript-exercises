c4581e07e5ede7bb1f438fc2c7844f1d5396b61c
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-02 22:17:25 +0200

    Create solution for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+	let sum = 0;
+
+  for (const value of numberArray) {
+    sum += value;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numberArray) {
+  let product = 1;
 
+  for (const value of numberArray) {
+    product *= value;
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number < 0) return "ERROR";
+
+	if (number === 0) {
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+  }
+  
 };
 
 // Do not edit below this line
