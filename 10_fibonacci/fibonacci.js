53ef10494a899b217881c20d4d3911e9666f48ba
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-03 15:05:39 +0200

    Create solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (typeof number === "string") number = Number(number);
+
+    if (number < 0) return "OOPS";
+
+    if (number === 0) return 0;
+
+    if (number === 1 || number === 2) return 1;
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
