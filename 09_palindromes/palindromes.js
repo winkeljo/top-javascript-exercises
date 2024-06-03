18e5b8943ab54ea5d27e7bd01573047d7cb29262
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-03 14:52:09 +0200

    Create solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (palindomeToCheck) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    stringArray = palindomeToCheck.toLowerCase().split("").filter((character) => alphanumerical.includes(character));
+    reversedArray = palindomeToCheck.toLowerCase().split("").filter((character) => alphanumerical.includes(character)).reverse();
+
+    if(stringArray.toString() === reversedArray.toString()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
