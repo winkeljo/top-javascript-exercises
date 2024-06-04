1d90c743144379d12e306500910bdeec4a8a6306
JoshiW <66128220+winkeljo@users.noreply.github.com>
2024-06-04 11:03:44 +0200

    Create solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const year = new Date().getFullYear()
+    let oldestPerson = {}
+    let oldestAge = 0
+
+    for (const person of people) {
+        let age = 0;
+
+        if (person.yearOfDeath === undefined) {
+            age = year - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
